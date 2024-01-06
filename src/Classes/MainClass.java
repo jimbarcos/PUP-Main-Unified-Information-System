afaaefa812e1cd8137b09d4e3b94958c44bda231
Jim Barcos <jimbarcos01@gmail.com>
2024-01-06 22:16:38 +0800

    File Upload
    Created S&T eval

---
+++ src/Classes/MainClass.java
@@ -0,0 +1,9 @@
+package Classes;
+
+public class MainClass {
+    public static void main(String[] args) {
+        SignIn logFrame = new SignIn();
+        logFrame.pack();
+        logFrame.setVisible(true); // Display LogIn frame
+    }
+}
